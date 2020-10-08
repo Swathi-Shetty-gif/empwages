692852e59905e7d741e59851562d47be75b544e2
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-09 00:01:18 +0530

    uc1

--- employee.sh
+++ employee.sh
@@ -1,2 +1,12 @@
-#!/bin/bash -x
+!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch
+#!bin/bash -x
+
+isPresent =1
+randomCheck=$((RANDOM%2))
+if [ $randomCheck  == $isPresent ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
